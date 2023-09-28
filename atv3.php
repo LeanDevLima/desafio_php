ccaa41a75263f98131edbabc8c51a511a3230ca7
Leanderson Lima <leanderson.devlima@gmail.com>
2023-09-28 06:17:02 -0300

    atv3

--- atv3.php
+++ atv3.php
@@ -17,19 +17,20 @@ while (true) {
             break;
         case 2:
             echo "Você escolheu a opção 2 - Alterar Aluno.\n";
-       
             break;
+
         case 3:
             echo "Você escolheu a opção 3 - Excluir Aluno.\n";
-          
             break;
+
         case 4:
             echo "Você escolheu a opção 4 - Mostrar todos os alunos.\n";
-            
             break;
+
         case 5:
             echo "Você escolheu a opção 5 - Sair. O programa será encerrado.\n";
             exit();
+            
         default:
             echo "Opção inválida. Por favor, escolha uma opção válida.\n";
     }
